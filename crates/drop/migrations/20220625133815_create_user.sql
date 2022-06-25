e3d5db3ed57f678bf850f52908974f30faf10eeb
Marshall Bowers <elliott.codes@gmail.com>
2022-06-25 13:05:29 -0400

    Add missing indexes

--- crates/drop/migrations/20220625133815_create_user.sql
+++ crates/drop/migrations/20220625133815_create_user.sql
@@ -2,6 +2,6 @@ create table user (
     id text primary key not null,
     created_at text not null,
     updated_at text not null,
-    username text not null,
+    username text not null unique,
     full_name text
 );
--- crates/drop/migrations/20220625133842_create_api_key.sql
+++ crates/drop/migrations/20220625133842_create_api_key.sql
@@ -3,6 +3,8 @@ create table api_key (
     created_at text not null,
     updated_at text not null,
     user_id text references user (id),
-    value text not null,
+    value text not null unique,
     expired_at text
 );
+
+create index idx_api_key_expire_at on api_key (expired_at);
