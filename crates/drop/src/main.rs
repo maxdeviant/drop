1d93c76824465bef54057e8be76e4e10cbb3f5d5
Marshall Bowers <elliott.codes@gmail.com>
2022-06-25 09:22:15 -0400

    Setup basic Rocket server

--- crates/drop/src/main.rs
+++ crates/drop/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "滴"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let rocket = rocket::build().mount("/", routes![index]).ignite().await?;
+
+    let _ = rocket.launch().await?;
+
+    Ok(())
 }
